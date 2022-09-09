07e57edd9b49e71a45b4f3bbb9d481dd8486d815
haustin71 <haustin71@charlottesvilleschools.org>
2022-09-09 09:55:47 -0400

    change code

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ import neopixel
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
 dot.brightness = 0.5 
 
-print("Make it red!")
+print("Make it Blue!")
 
 while True:
-    dot.fill((0, 0, 255))
+    dot.fill((255, 0, 0))
