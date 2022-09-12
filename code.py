174478d390510ed6983118789bdbc7a1d7522494
haustin71 <haustin71@charlottesvilleschools.org>
2022-09-12 09:52:56 -0400

    made the neopixel rgb

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 
 import board
 import neopixel
+import time
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
 dot.brightness = 0.5 
@@ -8,4 +9,10 @@ dot.brightness = 0.5
 print("Make it Blue!")
 
 while True:
-    dot.fill(0, 0, 255)
+    dot.fill((255, 0, 0))
+    time.sleep(1.0)
+    dot.fill((0, 255, 0))
+    time.sleep(1.0)
+    dot.fill((0, 0, 255))
+    time.sleep(1.0)
+   
