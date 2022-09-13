4ada87b51b9ba0ef7c32c8cf70526b520b9963d8
haustin71 <haustin71@charlottesvilleschools.org>
2022-09-13 10:36:29 -0400

    changed code

--- code.py
+++ code.py
@@ -2,7 +2,7 @@
 import time
 import board
 import pwmio
-from adafruit_motor import servo
+import servo
 
 pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
 
@@ -10,9 +10,9 @@ pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
 my_servo = servo.Servo(pwm)
 
 while True:
-    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-        my_servo.angle = angle
-        time.sleep(0.05)
-    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
-        my_servo.angle = angle
-        time.sleep(0.05)
+    for angle in range(0, 180, 10):  # 0 - 180 degrees, 5 degrees at a time.
+        my_servo.angle = 180
+        time.sleep(0.08)
+    for angle in range(180, 0, -10): # 180 - 0 degrees, 5 degrees at a time.
+        my_servo.angle = 0
+        time.sleep(0.08)
