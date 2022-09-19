c3edf66e9fb4a4d18726d64db5107245f470fc62
haustin71 <haustin71@charlottesvilleschools.org>
2022-09-19 09:55:36 -0400

    changed code

--- code.py
+++ code.py
@@ -2,12 +2,16 @@
 import time
 import board
 import adafruit_hcsr04
-
+import neopixel
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
-
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+dot.brightness = 0.5
+distance = sonar.distance
 while True:
     try:
         print((sonar.distance,))
     except RuntimeError:
         print("Retrying!")
     time.sleep(0.1)
+    if sonar.distance < 5:
+    dot.fill((255, 0, 0))
