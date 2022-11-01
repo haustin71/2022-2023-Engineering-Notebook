1da70579c613339451b52f9383962c29a3b4ce8a
haustin71 <haustin71@charlottesvilleschools.org>
2022-11-01 10:40:11 -0400

    changed code

--- LCD with Buttons.py
+++ LCD with Buttons.py
@@ -1,55 +1,54 @@
 
-import time
 import board
+import time
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 from digitalio import DigitalInOut, Direction, Pull
 
 # get and i2c object
-
 i2c = board.I2C()
-btn = DigitalInOut(board.D1)
+btn = DigitalInOut(board.D3)
+btn2 = DigitalInOut(board.D2)
 btn.direction = Direction.INPUT
 btn.pull = Pull.UP
-btn2 = DigitalInOut(board.D2)
 btn2.direction = Direction.INPUT
 btn2.pull = Pull.UP
-countval = 0
-Btn_State = True
-PrevBtn_State = True
-Btn2_State = True
-PrevBtn2_State = True
 # some LCDs are 0x3f... some are 0x27.
-lcd = LCD(I2CPCF8574Interface(i2c, 0x3f), num_rows=2, num_cols=16)
+lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
+cur_state = True
+prev_state = True
+cur_state2 = True
+prev_state2 = True
+buttonPress = 0
 
 while True:
     while btn2.value == False:
-        lcd.set_cursor_pos(0, 0)
-        lcd.print("countval up")
-        Btn_State = btn.value
-        if Btn_State != PrevBtn_State:
-            if not Btn_State:
-                countval = countval + 1 
+        lcd.set_cursor_pos(1,0)
+        lcd.print("UP  ")
+        cur_state = btn.value
+        if cur_state != prev_state:
+            if not cur_state:
+                buttonPress = buttonPress + 1
                 lcd.clear()
-                lcd.set_cursor_pos(0, 0)
-                lcd.print(str(countval))
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
             else:
                 lcd.clear()
                 lcd.set_cursor_pos(0,0)
-                lcd.print(str(countval))
-        PrevBtn_State = Btn_State
+                lcd.print(str(buttonPress))
+        prev_state = cur_state
     else:
         lcd.set_cursor_pos(1,0)
-        lcd.print("countval down")
-        Btn2_State = btn2.value
-        if Btn2_State != PrevBtn2_State:
-            if not Btn2_State:
-                    countval = countval - 1
-                    lcd.clear()
-                    lcd.set_cursor_pos(0,0)
-                    lcd.print(str(countval))
+        lcd.print("DOWN")
+        cur_state2 = btn.value
+        if cur_state2 != prev_state2:
+            if not cur_state2:
+                buttonPress = buttonPress - 1
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
             else:
                 lcd.clear()
                 lcd.set_cursor_pos(0,0)
-                lcd.print(str(countval))
-        PrevBtn2_State = Btn2_State
+                lcd.print(str(buttonPress))
+        prev_state2 = cur_state2
--- code.py
+++ code.py
@@ -1,41 +1,10 @@
 
-import time
+from unittest.mock import MagicProxy
 import board
-from lcd.lcd import LCD
-from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+import time
 from digitalio import DigitalInOut, Direction, Pull
-countval = 0
-# get and i2c object
-
-i2c = board.I2C()
-btn = DigitalInOut(board.D1)
-btn.direction = Direction.INPUT
-btn.pull = Pull.UP
-btn2 = DigitalInOut(board.D2)
-btn2.direction = Direction.INPUT
-btn2.pull = Pull.UP
-# some LCDs are 0x3f... some are 0x27.
-lcd = LCD(I2CPCF8574Interface(i2c, 0x3f), num_rows=2, num_cols=16)
-
-while True:
-       if not btn2.value:
-        if not btn.value:
-            lcd.clear()
-            lcd.set_cursor_pos(0, 0)
-            lcd.print("countval")
-            lcd.set_cursor_pos(0,13)
-            countval = countval + 1
-            lcd.print(str(countval))
-        else:
-            pass
-    
-        if not btn2.value:
-            lcd.clear()
-            lcd.set_cursor_pos(0, 0)
-            lcd.print("countval")
-            countval = countval - 1
-            lcd.set_cursor_pos(0,13)
-            lcd.print(str(countval))
-        else:
-            pass
-            time.sleep(0.1) # sleep for debounce
+from analogio import AnalogIn
+import pwmio
+import adafruit_motor.servo
+servo = adafruit_motor.servo.Servo(pwm)
+pwm = pwmio.PWMOut(board.D8, frequency=50)
